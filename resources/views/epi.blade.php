ab2e5f0e0db0864d40b26ad19ff45ed4b166e1cb
Epiphane Niada <epiphaneniada@gmail.com>
2022-09-05 09:40:32 +0100

    premier

---
+++ resources/views/epi.blade.php
@@ -0,0 +1,67 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>01</title>
+	<link rel="stylesheet" href="aimeosa.css">
+</head>
+<body>
+    <h1>Bonjour le monde</h1>
+    
+
+
+
+
+
+
+    <footer>
+			<div class="container-fluid">
+				<div class="row">
+					<div class="col-md-8">
+						<div class="row">
+							<div class="col-sm-6 footer-left">
+								<div class="footer-block">
+									<h2 class="pb-3">LEGAL</h2>
+									<p><a href="http://127.0.0.1:8000/p/p/terms">Terms &amp; Conditions</a></p>
+									<p><a href="http://127.0.0.1:8000/p/p/privacy">Privacy Notice</a></p>
+									<p><a href="http://127.0.0.1:8000/p/p/cancel">Cancellation</a></p>
+								</div>
+							</div>
+							<div class="col-sm-6 footer-center">
+								<div class="footer-block">
+									<h2 class="pb-3">ABOUT US</h2>
+									<p><a href="http://127.0.0.1:8000/p/p/contact">Contact us</a></p>
+									<p><a href="http://127.0.0.1:8000/p/p/about">Company</a></p>
+								</div>
+							</div>
+						</div>
+					</div>
+					<div class="col-md-4 footer-right">
+						<div class="footer-block">
+							<a class="logo" href="/" title="Logo">
+							    <img src="http://127.0.0.1:8000/aimeos/1.d/logo.png" height="40" title="Logo">
+							</a>
+							<div class="social">
+								<p><a href="#" class="sm facebook" title="Facebook" rel="noopener">Facebook</a></p>
+								<p><a href="#" class="sm twitter" title="Twitter" rel="noopener">Twitter</a></p>
+								<p><a href="#" class="sm instagram" title="Instagram" rel="noopener">Instagram</a></p>
+								<p><a href="#" class="sm youtube" title="Youtube" rel="noopener">Youtube</a></p>
+							</div>
+						</div>
+					</div>
+				</div>
+			</div>
+		</footer>
+
+
+
+
+</body>
+<script src="http://127.0.0.1:8000/vendor/shop/themes/default/aimeos.js?v=1"></script>
+<script src="aimeosa.js"></script>
+<style>
+	
+</style>
+</html>
